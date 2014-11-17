1345f29acc11849c83fec5120e75b6a4b220f718
doctea <devnull@localhost>
2014-11-17 22:04:46 +0000

    wowweee whole buncha changes/fixes to ChainSequence and stuffs... better reuse of Sequences and tings... still ongoing

--- src/vurfeclipse/user/scenes/OutputFX2.java
+++ src/vurfeclipse/user/scenes/OutputFX2.java
@@ -7,7 +7,9 @@ import vurfeclipse.filters.KaleidoFilter;
 import vurfeclipse.filters.ShaderFilter;
 import vurfeclipse.projects.Project;
 import vurfeclipse.scenes.*;
+import vurfeclipse.sequence.ChainSequence;
 import vurfeclipse.sequence.Sequence;
+import vurfeclipse.sequence.ShowFilterSequence;
 import vurfeclipse.user.scenes.OutputFX1.OutputSequence1;
  
 public class OutputFX2 extends SimpleScene {
@@ -62,6 +64,12 @@ public class OutputFX2 extends SimpleScene {
 
     public void setupSequences() {
 		sequences.put("preset 1", new OutputSequence1(this, 0));
+		sequences.put("show_kaleido", new ShowFilterSequence(this, 0, getPath()+"/fl/Kaleido"));
+		sequences.put("show_feedback", new ChainSequence(2000)
+			//.addSequence(getSequence("show_feedback"))
+			.addSequence(host.getSceneForPath("/sc/BlankerScene").getSequence("feedback"))
+			.addSequence(new ShowFilterSequence(this, 0, getPath()+"/fl/Feedback")));
+		//sequences.put("show_blend", new ShowFilterSequence(this, 0, host.getSceneForPath(getPath()).getFilter("Kaleido")));
     }
 	
 
@@ -80,7 +88,7 @@ public class OutputFX2 extends SimpleScene {
 			//host.getFilter("BlendDrawer1").changeParameterValue("Opacity", (float)norm);
 		}
 		@Override public void onStart() {
-	   		if (APP.getApp().random(0f,1.0f)>=0.5f) {
+	   		/*if (APP.getApp().random(0f,1.0f)>=0.5f) {
     			host.host.getSceneForPath(getPath()).getFilter("Feedback").setMuted(false);
     			host.host.getSceneForPath("/sc/BlankerScene")
     				.getFilter("BlankFilter")
@@ -90,8 +98,8 @@ public class OutputFX2 extends SimpleScene {
     			host.host.getSceneForPath("/sc/BlankerScene")
     				.getFilter("BlankFilter")
     				.changeParameterValue("alpha", (int)255);			
-    		}
-    		if (APP.getApp().random(0f,1.0f)>=0.5f) host.host.getSceneForPath(getPath()).getFilter("Kaleido").toggleMute();
+    		}*/
+    		//if (APP.getApp().random(0f,1.0f)>=0.5f) host.host.getSceneForPath(getPath()).getFilter("Kaleido").toggleMute();
     		
     		//if (APP.getApp().random(0f,1.0f)>=0.5f) host.host.getSceneForPath("/sc/OutputShader2").getFilter("BlendDrawer pix0 to out").toggleMute();
     		host.host.getSceneForPath(getPath()).getFilter("BlendDrawer pix0 to out").setMuted((APP.getApp().random(0f,1.0f)>=0.25f));
@@ -102,6 +110,31 @@ public class OutputFX2 extends SimpleScene {
     		host.host.getSceneForPath(getPath()).getFilter("Kaleido").nextMode();    			
 		}
 		@Override public void onStop() {	}
-	}		
+	}
+	
+	class FeedbackSequence1 extends Sequence {
+		public FeedbackSequence1(OutputFX2 outputFX2, int i) {
+			// TODO Auto-generated constructor stub
+			super(outputFX2, i);
+		}
+
+		@Override
+		public void setValuesForNorm(double pc, int iteration) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStart() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStop() {
+			// TODO Auto-generated method stub
+			
+		}		
+	}
 	
 }
