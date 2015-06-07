c1b7635fca48a165709907e35a62b3f6c5c142bf
doctea <devnull@localhost>
2015-06-08 00:24:15 +0100

    nice, no really

---
+++ src/data/SyncEffect.glsl
@@ -0,0 +1,34 @@
+#define PI 3.141592653589793
+
+uniform sampler2D src_tex_unit0;
+
+uniform float step_x;
+uniform float step_y;
+
+void main(void)
+{
+    vec2 tex_coord = gl_TexCoord[0].st;
+
+    //vec4 ts = textureSize2D(src_tex_unit0);
+    /*if (mirror_x==1 && gl_TexCoord[0].x > 0.5) {
+      tex_coord.xy = vec2(1.0-tex_coord.x, tex_coord.y);
+    }
+    if (mirror_y==1 && gl_TexCoord[0].y > 0.5) {
+      tex_coord.xy = vec2(tex_coord.x, 1.0-tex_coord.y);
+    }*/
+    
+    //tex_coord.xy = vec2(tex_coord.x * step_x, tex_coord.y * step_y);
+    
+    //float ang = atan2(tex_coord.xy);
+    //float d = distance(vec2(0.0,0.0),tex_coord.xy);  
+    
+    //tex_coord.x = sin(ang) * (d + step_x);
+    //tex_coord.y = cos(ang) * (d + step_x);
+    
+    ////new_coord.x = sin(cos(tex_coord.x) * step_x);
+    ////new_coord.y = cos(tan(tex_coord.y) * step_y);
+    //tex_coord.xy = new_coord.xy;
+    
+    gl_FragColor = vec4( texture2D(src_tex_unit0, tex_coord).rgba );
+    
+}
