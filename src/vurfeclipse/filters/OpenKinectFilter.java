c540b8e12e971480e476fbe92ad1100ee6a906ac
doctea <devnull@localhost>
2017-11-16 22:53:20 +0000

    remove nonworking pointcloud controls from libfreenect

--- src/vurfeclipse/filters/OpenKinectFilter.java
+++ src/vurfeclipse/filters/OpenKinectFilter.java
@@ -40,12 +40,12 @@ public class OpenKinectFilter extends Filter {
 		    super.setParameterDefaults();
 		    this.addParameter("rgb", new Boolean(true));//, 1.0f, 5.0f);
 		    this.addParameter("depth", new Boolean(true)); //(0), -sc.w/2, sc.w/2);
-		    this.addParameter("pointCloud", new Boolean(false)); //(0), -sc.w/2, sc.w/2);
+		    /*this.addParameter("pointCloud", new Boolean(false)); //(0), -sc.w/2, sc.w/2);
 		    
 		    this.addParameter("pc_rot_x", new Integer(0), new Integer(0), new Integer(360));
 		    this.addParameter("pc_rot_y", new Integer(0), new Integer(0), new Integer(360));
 		    this.addParameter("pc_rot_z", new Integer(0), new Integer(0), new Integer(360));
-		    this.addParameter("pc_zoom", new Integer(-1600), new Integer(-10000), new Integer(10000));
+		    this.addParameter("pc_zoom", new Integer(-1600), new Integer(-10000), new Integer(10000));*/
 		    
 		    //this.addParameter("ir", new Boolean(false)); //(0), -sc.w/2, sc.w/2);
 		    //this.addParameter("translate_y", new Integer(0), -sc.h/2, sc.h/2);
@@ -56,18 +56,18 @@ public class OpenKinectFilter extends Filter {
 		  }
 
 	@Override
-	  public boolean initialise () {
-	    //setParameterDefaults();
+	public boolean initialise () {
+		//setParameterDefaults();
 		//this.setMuted(this.muted);	// 2017-10-29
-		
+
 		kinect = new Kinect(APP.getApp());
 		kinect.activateDevice(this.device);
 		kinect.initDepth();
 		kinect.initVideo();
 		kinect.enableColorDepth(true);
-		
-	    return true;
-	  }
+
+		return true;
+	}
 
 
 		@Override
