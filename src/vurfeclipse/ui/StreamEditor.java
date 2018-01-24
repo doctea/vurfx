91a2c2f7642668f4ea95842c79d270355148a473
doctea <devnull@localhost>
2018-01-24 22:52:40 +0000

    missed file

---
+++ src/vurfeclipse/ui/StreamEditor.java
@@ -0,0 +1,54 @@
+package vurfeclipse.ui;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import controlP5.Accordion;
+import controlP5.ControlP5;
+import controlP5.Group;
+import vurfeclipse.sequencers.Sequencer;
+import vurfeclipse.streams.Stream;
+
+public class StreamEditor extends Group {
+	Accordion editor;
+	
+	public StreamEditor(ControlP5 theControlP5, String theName) {
+		super(theControlP5, theName);
+		
+	}
+
+	public Accordion setupStreamEditor(ControlFrame cf, HashMap<String,Stream> streams) {
+		editor = cf.control().addAccordion("streams_editor").setWidth(cf.displayWidth/3).moveTo(this);
+
+		//Scene n;
+		for (Entry<String, Stream> i : streams.entrySet()) {
+			//String tabName = "["+c+"] " + n.getSceneName(); //getClass();
+			//ControlP5 cp5 = ((VurfEclipse)APP.getApp()).getCP5();
+			//Tab tab = cp5.addTab(tabName);
+
+			String streamName = i.getKey();
+			Group g = cf.control().addGroup(streamName);
+
+			//println("added tab " + tabName);
+			//ControllerInterface[] controls = ((Scene)i.next()).getControls();
+			//cp5.begin(10,40);
+			//((Scene)n).setupControls(cf,g);//tab);
+			i.getValue().setupControls(cf,g);
+			//println("done setupControls for " + i.getValue());
+			//cp5.end();
+
+			editor.addItem(g);
+
+						/*for (int n = 0 ; n < controls.length ; n++) {
+			    cp5.getTab("Scene " + c).add(controls[n]).moveTo("Scene " + c);
+			    //cp5.addSlider(controls[n]).moveTo("Scene " + c);
+			  }*/
+			//c++;
+			//((Scene)i).setupControls(cp5);
+		}		
+		
+		editor.open().setCollapseMode(Accordion.MULTI);
+		
+		return editor;
+	}
+}
