d40001c614b5c116bfcc8d387626c235a67b5d06
doctea <devnull@localhost>
2018-02-05 10:56:23 +0000

    minor tidyup

--- src/vurfeclipse/filters/WebcamFilter.java
+++ src/vurfeclipse/filters/WebcamFilter.java
@@ -13,98 +13,83 @@ import processing.video.*;
 
 public class WebcamFilter extends Filter {
 
-  boolean pixelMode = true; // false mode doesnt work?
-  
-  //int capW, capH;
-  
-  int cameraNumber = 0;
+	boolean pixelMode = true; // false mode doesnt work?
 
-  transient Capture webcamStream;
-  
-  transient PGraphics tex;
-  
-  WebcamFilter(Scene sc, int capW, int capH, int cameraNumber) {
-    this(sc, capW, capH);
-    this.cameraNumber = cameraNumber; 
-  }
-  public WebcamFilter(Scene sc, int capW, int capH) {
-    super(sc);
-    this.capW = capW;
-    this.capH = capH;
-  }
-  public WebcamFilter(Scene sc) {
-    this(sc, sc.w, sc.h);
-  }
-  public WebcamFilter(Scene sc, int cameraNumber) {
-    this(sc);
-    this.cameraNumber = cameraNumber;
-  }
-  
-  int capW = 640, capH = 480;
-  
-  public boolean initialise() {
-      webcamStream = new Capture(APP.getApp(), capW, capH); //, cameraName); //, global_fps);
-      //webcamStream = new GSCapture(APP, 640, 480, cameraName); //, global_fps);      
-      //webcamStream = new GSCapture(APP, capW, capH, global_fps);
-      
-      System.out.println("WebcamFilter initialised for cameraNumber: " + this.cameraNumber);
-      System.out.println("WebcamFilter OUT is " + out);
-      
-      webcamStream.start();
-      System.out.println("webcam initialise?");
-      //APP.exit();
-      return true;
-  }	  
-  
-  public boolean applyMeatToBuffers() {
-    //System.out.println("applyMeatToBuffers()");
-    if (webcamStream!=null && webcamStream.available()) {
-      //System.out.println("got webcamstream");
-      //webcamStream.read();
-      
-      /*if (!pixelMode) {
-        webcamStream.loadPixels();
-        //out.loadPixels();
-        //out.pixels = webcamStream.pixels;
-        //arrayCopy(webcamStream.pixels, out.pixels);
-        //out.getTexture().putBuffer(webcamStream.pixels);
-      } else {
-        //if (tex.putPixelsIntoTexture()) {
-    	if (tex.isModified()) {
-    	  tex.updatePixels();*/
-      		//out.scale(1.0f);
-      if (webcamStream.available() == true) {
-    	  webcamStream.read();
-          out.beginDraw();
-          //out.scale(1.0f); //-1,-1);
-          out.pushMatrix();
-          //out.scale(-1.0f, 1.0f);
-          //out.image(webcamStream,0,0,sc.w,sc.h);
-          out.set(0, 0,  webcamStream);
-          out.popMatrix();
-          out.endDraw();
-        /*}
-      }*/
-      }
-      return true;
-    } else {
-      return false;  // didn't get a new frame so return false
-    }
-  }  
-  
-  public void beginDraw () {
-    /*if (!pixelMode)
+	//int capW, capH;
+
+	int cameraNumber = 0;
+
+	transient Capture webcamStream;
+
+	transient PGraphics tex;
+
+	WebcamFilter(Scene sc, int capW, int capH, int cameraNumber) {
+		this(sc, capW, capH);
+		this.cameraNumber = cameraNumber; 
+	}
+	public WebcamFilter(Scene sc, int capW, int capH) {
+		super(sc);
+		this.capW = capW;
+		this.capH = capH;
+	}
+	public WebcamFilter(Scene sc) {
+		this(sc, sc.w, sc.h);
+	}
+	public WebcamFilter(Scene sc, int cameraNumber) {
+		this(sc);
+		this.cameraNumber = cameraNumber;
+	}
+
+	int capW = 640, capH = 480;
+
+	public boolean initialise() {
+		webcamStream = new Capture(APP.getApp(), capW, capH); //, cameraName); //, global_fps);
+		//webcamStream = new GSCapture(APP, 640, 480, cameraName); //, global_fps);      
+		//webcamStream = new GSCapture(APP, capW, capH, global_fps);
+
+		System.out.println("WebcamFilter initialised for cameraNumber: " + this.cameraNumber);
+		System.out.println("WebcamFilter OUT is " + out);
+
+		webcamStream.start();
+		System.out.println("webcam initialise?");
+		//APP.exit();
+		return true;
+	}	  
+
+	public boolean applyMeatToBuffers() {
+		//System.out.println("applyMeatToBuffers()");
+		if (webcamStream!=null && webcamStream.available()) {
+			//System.out.println("got webcamstream");
+			if (webcamStream.available() == true) {
+				webcamStream.read();
+				out.beginDraw();
+				//out.scale(1.0f); //-1,-1);
+				out.pushMatrix();
+				//out.scale(-1.0f, 1.0f);
+				//out.image(webcamStream,0,0,sc.w,sc.h);
+				out.set(0, 0,  webcamStream);
+				out.popMatrix();
+				out.endDraw();
+			}
+			return true;
+		} else {
+			return false;  // didn't get a new frame so return false
+		}
+	}  
+
+	public void beginDraw () {
+		/*if (!pixelMode)
       out.loadPixels();
     else*/
-      //out.beginDraw();
-  }
-  public void endDraw () {
-    /*if (!pixelMode)
+		//out.beginDraw();
+	}
+	public void endDraw () {
+		/*if (!pixelMode)
       out.updatePixels();
     else {*/
-      //out.endDraw();
-      //out.loadPixels();
-      //out.loadTexture();
-    //}
-  }
+		//out.endDraw();
+		//out.loadPixels();
+		//out.loadTexture();
+		//}
+	}
 }
