ab067071d98c47bb3e4dbed3e05af9cd570aef5a
doctea <devnull@localhost>
2018-04-02 00:31:31 +0100

    can select webcam camera number.. but it doesnt quite work rightg, dynamia siwtching breaks it too

--- src/vurfeclipse/filters/WebcamFilter.java
+++ src/vurfeclipse/filters/WebcamFilter.java
@@ -18,6 +18,9 @@ public class WebcamFilter extends Filter {
 	//int capW, capH;
 
 	int cameraNumber = 0;
+	
+	int mode = 0;
+	int maxModes = Capture.list().length;
 
 	transient Capture webcamStream;
 
@@ -41,11 +44,43 @@ public class WebcamFilter extends Filter {
 	}
 
 	int capW = 640, capH = 480;
+	
+	  public void setParameterDefaults () {
+		    super.setParameterDefaults();
+		    //println("setting defaults");
+		    //this.changeParameterValue("Opacity", 1.0);
+		    //this.changeParameterValue("BlendMode", 4);
+		    addParameter("camera_number", new Integer(0), 0, Capture.list().length);
+	  }
 
 	public boolean initialise() {
-		  super.initialise();
+		super.initialise();
+		
+		this.initialiseWebcam();
+		//APP.exit();
+		return true;
+	}	  
+	
+	@Override
+	public Filter nextMode() {
+		int orig = cameraNumber;
+		cameraNumber++;
+		if (cameraNumber>maxModes) cameraNumber = 0;
+		if (cameraNumber!=orig) {
+			this.changeParameterValue("camera_number", cameraNumber);
+			//this.initialiseWebcam();
+		}
+		return this;
+	}
 
-		webcamStream = new Capture(APP.getApp(), capW, capH); //, cameraName); //, global_fps);
+	private void initialiseWebcam() {
+		if (this.webcamStream!=null) this.webcamStream.stop();
+		this.webcamStream = null;
+
+		if (this.cameraNumber>=Capture.list().length) 
+			return;		
+
+		webcamStream = new Capture(APP.getApp(), capW, capH, Capture.list()[this.cameraNumber]); //, this.cameraNumber); //, cameraName); //, global_fps);
 		//webcamStream = new GSCapture(APP, 640, 480, cameraName); //, global_fps);      
 		//webcamStream = new GSCapture(APP, capW, capH, global_fps);
 
@@ -54,26 +89,34 @@ public class WebcamFilter extends Filter {
 
 		webcamStream.start(); //TODO: why doesn't this compile on macosx..?
 		//webcamStream.run();
-		System.out.println("webcam initialise?");
-		//APP.exit();
-		return true;
-	}	  
-
+		System.out.println("webcam initialise?");		
+	}
+	synchronized public void updateParameterValue(String paramname, Object value) {
+		if (paramname.equals("camera_number")) {
+			if (this.cameraNumber!=(int)value) {
+				this.cameraNumber = (int)value;
+				webcamStream = null;
+				this.initialiseWebcam();
+			}
+		}
+	}
+	
 	public boolean applyMeatToBuffers() {
 		//System.out.println("applyMeatToBuffers()");
-		if (webcamStream!=null && webcamStream.available()) {
+		if (webcamStream!=null) { // && webcamStream.available()) {
 			//System.out.println("got webcamstream");
 			if (webcamStream.available() == true) {
 				webcamStream.read();
+			}
 				out().beginDraw();
 				//out.scale(1.0f); //-1,-1);
 				out().pushMatrix();
 				//out.scale(-1.0f, 1.0f);
-				//out.image(webcamStream,0,0,sc.w,sc.h);
+				//out().image(webcamStream,0,0,sc.w,sc.h);
 				out().set(0, 0,  webcamStream);
 				out().popMatrix();
 				out().endDraw();
-			}
+			
 			return true;
 		} else {
 			return false;  // didn't get a new frame so return false
