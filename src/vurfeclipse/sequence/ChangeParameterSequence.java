69a743d837339c8c779cdc2983952e304231eeef
doctea <devnull@localhost>
2018-04-21 20:09:47 +0100

    rearrange gui, changeparametersequence can latch and convert pc to time

--- src/vurfeclipse/sequence/ChangeParameterSequence.java
+++ src/vurfeclipse/sequence/ChangeParameterSequence.java
@@ -14,6 +14,7 @@ import controlP5.ControlP5;
 import controlP5.ScrollableList;
 import controlP5.Textfield;
 import controlP5.Textlabel;
+import controlP5.Toggle;
 import vurfeclipse.APP;
 import vurfeclipse.Targetable;
 import vurfeclipse.VurfEclipse;
@@ -30,12 +31,22 @@ import com.udojava.evalex.AbstractFunction;
 import com.udojava.evalex.Expression.*;
 
 public class ChangeParameterSequence extends Sequence {
-	
+
 	com.udojava.evalex.Expression evaluator;
-	
+
 	//String filterPath, parameterName;
 	String expression = "input";
 	String targetPath;
+	
+	boolean convertPCtoTime = false;
+
+	public boolean isConvertPCtoTime() {
+		return convertPCtoTime;
+	}
+
+	public void setConvertPCtoTime(boolean convertPCtoTime) {
+		this.convertPCtoTime = convertPCtoTime;
+	}
 
 	public String getTargetPath() {
 		return targetPath;
@@ -46,12 +57,12 @@ public class ChangeParameterSequence extends Sequence {
 	}
 
 	Object value;
-	
+
 	ParameterBuffer paramBuffer;
 
 	private int outputMode;
 
-	
+
 	public int getOutputMode() {
 		return outputMode;
 	}
@@ -75,7 +86,7 @@ public class ChangeParameterSequence extends Sequence {
 		super(); 
 		this.paramBuffer = new ParameterBuffer(10,50);
 	}
-	
+
 	public ChangeParameterSequence(Scene host, String filterPath, String parameterName, Object startValue, String expression, int length) {
 		//super(host,length);
 		super(host,length);
@@ -84,15 +95,15 @@ public class ChangeParameterSequence extends Sequence {
 		this.targetPath = filterPath + "/pa/" + parameterName;
 		this.value = value;
 		this.setExpression(expression);
-		
+
 		this.paramBuffer = new ParameterBuffer(10,50);
-		
+
 	}
 	public ChangeParameterSequence(Scene host, String filterPath, String parameterName, Object value, int length) {	// compatibility constructor
 		//super(host,length);
 		this(host,filterPath,parameterName,value,"input",length); 
 	}
-	
+
 	@Override
 	public HashMap<String,Object> collectParameters() {
 		HashMap<String,Object> params = super.collectParameters();
@@ -102,13 +113,15 @@ public class ChangeParameterSequence extends Sequence {
 		params.put("value", this.value);
 		params.put("expression", this.expression);
 		params.put("outputMode", this.getOutputMode());
+		params.put("converttotime", this.isConvertPCtoTime());
+		params.put("latching", this.isLatching());
 		return params;
 	}
-	
+
 	@Override
 	public void loadParameters(HashMap<String,Object> params) {
 		super.loadParameters(params);
-		
+
 		//compatibility
 		if (params.containsKey("filterPath"))	{	// for old projects
 			this.targetPath = (String) params.get("filterPath") + "/pa/" + (String) params.get("parameterName");
@@ -118,6 +131,8 @@ public class ChangeParameterSequence extends Sequence {
 		if (params.containsKey("value")) 		this.value = params.get("value");
 		if (params.containsKey("expression")) 	this.setExpression((String) params.get("expression"));
 		if (params.containsKey("outputMode")) 	this.setOutputMode((Integer) params.get("outputMode"));
+		if (params.containsKey("converttotime")) 	this.setConvertPCtoTime((Boolean) params.get("converttotime"));
+		if (params.containsKey("latching")) 	this.setLatching((Boolean) params.get("latching"));
 	}
 
 	@Override public ArrayList<Mutable> getMutables () {
@@ -133,37 +148,41 @@ public class ChangeParameterSequence extends Sequence {
 		//this.mutables.add(host);			
 		return this.mutables;
 	}
-	
+
 	@Override
 	public void start() {
 		if (host.host.getObjectForPath(targetPath) instanceof Parameter) {
 			this.paramBuffer.setCircular((((Parameter)host.host.getObjectForPath(targetPath)).isCircular()));
 		}
 	}
-	
+
 	@Override
 	synchronized public void __setValuesForNorm(double pc, int iteration) {
-				
+
 		if (((Double)pc).isNaN()) {
 			println("Caught NaN in __setValuesForNorm ");
 		}
 		// evaluate value to pass based on expression
 		if (evaluator==null) evaluator = APP.getApp().makeEvaluator(expression);
+
+		if (this.isConvertPCtoTime()) {
+			pc = Math.floor(pc * this.getLengthMillis()/100.0d);
+		}
 		
 		evaluator.setVariable("input", BigDecimal.valueOf(pc));
 		evaluator.setVariable("iteration", BigDecimal.valueOf(iteration));
 		BigDecimal value = evaluator.eval();
 		if (value==null)
 			println("caught null returned from eval(input = " + pc + ", iteration = " + iteration + ")");
-		
+
 		this.updateGuiInputValue("iter# " + iteration + " | pc: " + (float)(pc)); //Float.parseFloat(pc));
-		
+
 		//println(this + ": got value " + value);
-		
-		if (paramBuffer!=null) value = (BigDecimal) paramBuffer.getValue(value, false);	// lerp
-		
+
+		if (paramBuffer!=null) value = (BigDecimal) paramBuffer.getValue(value, latching);	// lerp
+
 		this.updateGuiOutputValue(value.toString());
-		
+
 		/*((Filter)host.host
 				.getObjectForPath(filterPath))
 				//.changeParameterValueFromSin(parameterName, (float)value.doubleValue());//(float)Math.sin(value.doubleValue()));		
@@ -176,16 +195,16 @@ public class ChangeParameterSequence extends Sequence {
 		} else if (this.getOutputMode()==Parameter.OUT_SIN) {
 			t.target(targetPath.replace("/pa/", "/ps/"), value.floatValue());			
 		}
-		
+
 	}
 
 	private void updateGuiOutputValue(final String value) {
 		/*APP.getApp().getCF().queueUpdate(new Runnable() {
 			@Override
 			public void run() {*/
-				if (lblOutputValue!=null)
-					lblOutputValue.setValueLabel(value);
-			/*}
+		if (lblOutputValue!=null)
+			lblOutputValue.setValueLabel(value);
+		/*}
 		});		*/
 	}
 
@@ -193,9 +212,9 @@ public class ChangeParameterSequence extends Sequence {
 		/*APP.getApp().getCF().queueUpdate(new Runnable() {
 			@Override
 			public void run() {*/
-				if (lblInputValue!=null)
-					lblInputValue.setValueLabel(value);
-			/*}
+		if (lblInputValue!=null)
+			lblInputValue.setValueLabel(value);
+		/*}
 		});		*/
 	}
 
@@ -203,7 +222,7 @@ public class ChangeParameterSequence extends Sequence {
 	public void onStart() {
 		if (value!=null)
 			((Targetable)host.host.getObjectForPath(targetPath)).target(targetPath, value);
-/*		((Filter)host.host
+		/*		((Filter)host.host
 			.getObjectForPath(filterPath))
 			.changeParameterValue(parameterName, value);*/
 	}
@@ -214,7 +233,7 @@ public class ChangeParameterSequence extends Sequence {
 			if (value!=null)
 				((Targetable)host.host.getObjectForPath(targetPath)).target(targetPath, value);
 
-				/*((Filter)host.host
+			/*((Filter)host.host
 					.getObjectForPath(filterPath))
 					.changeParameterValue(parameterName, value);*/
 		} catch (Exception e) {
@@ -222,17 +241,54 @@ public class ChangeParameterSequence extends Sequence {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private Textlabel lblInputValue;
 	private Textlabel lblOutputValue;
 	private ScrollableList lstOutputMode;
-	
+
+	private boolean latching;
+
+	public boolean isLatching() {
+		return latching;
+	}
+
 	@Override
 	public SequenceEditor makeControls(final ControlFrame cf, String name) {
 		// add an accordion to hold the sub-sequences and recurse
 		SequenceEditor sequenceEditor = super.makeControls(cf, name);
-		
+
 		final ChangeParameterSequence sequence = this;
+
+		Toggle tglLatching = cf.control().addToggle(name + "_latching")
+				.changeValue(this.isLatching()?1.0f:0.0f)/*.setLabel("enabled")*/
+				.setColorActive(VurfEclipse.makeColour(255, 255, 128))
+				.setLabel("Latch value")
+				.setPosition(500, 0)
+				.moveTo(sequenceEditor)
+				.addListenerFor(cf.control().ACTION_BROADCAST,  new CallbackListener() {
+					@Override
+					public void controlEvent(CallbackEvent theEvent) {
+						//sequence.setEnabled(theEvent.getController().getValue()==1.0f); //!self.isEnabled());
+						sequence.setLatching(theEvent.getController().getValue()==1.0f);
+					}
+				})
+			;
+			//pos_x += tglEnabled.getWidth() + margin_x;
+		Toggle tglConvertToTime = cf.control().addToggle(name + "_convert_to_time_")
+				.changeValue(this.isConvertPCtoTime()?1.0f:0.0f)/*.setLabel("enabled")*/
+				.setLabel("Use converted time instead of PC")
+				.setPosition(500 + tglLatching.getWidth()*2, 0)
+				.moveTo(sequenceEditor)
+				.addListenerFor(cf.control().ACTION_BROADCAST,  new CallbackListener() {
+					@Override
+					public void controlEvent(CallbackEvent theEvent) {
+						//sequence.setEnabled(theEvent.getController().getValue()==1.0f); //!self.isEnabled());
+						sequence.setConvertPCtoTime(theEvent.getController().getValue()==1.0f);
+					}
+				})
+			;
+			//pos_x += tglEnabled.getWidth() + margin_x;
+
 
 		int pos_x = 0, pos_y = 40, margin_x = 10, margin_y = 15;
 		SequenceEditor g = sequenceEditor;
@@ -242,132 +298,154 @@ public class ChangeParameterSequence extends Sequence {
 		lstParam.moveTo(g).close().setLabel("source");
 		lstParam.addItems(this.getStreamParams());//addItem(i.getKey(), i.getKey())
 		g.add(lstParam);*/
-		
+
 		//println ("adding gui for " + c);
 		//if (c instanceof FormulaCallback) {
-			Textfield txtExpression = cp5.addTextfield(name + "_Expression")
-					.setText(this.getExpression())
-					.setPosition(pos_x, pos_y)
-					.moveTo(g).setLabel("Expression")
-					.setAutoClear(false);
-			
-			pos_x += txtExpression.getWidth() + margin_x;
-			
-			// TODO: add callback handler to actually set expression
 
-			CallbackListener setExpression = new CallbackListener() {
-				public void controlEvent(CallbackEvent theEvent) {
-					//((ScrollableList)theEvent.getController()).close();
-					println("Setting new expression: " + ((Textfield)theEvent.getController()).getText());
-					setExpression(((Textfield)theEvent.getController()).getText());
-					((Textfield)theEvent.getController()).setValueLabel(sequence.getExpression());
-				}
-			};
-			txtExpression.addListenerFor(txtExpression.ACTION_BROADCAST,setExpression);
-			
-			g.add(txtExpression);
-			
-			
-			//tglOutputMode = cp5.addToggle(name + "_outputmode_toggle").setPosition(pos_x, pos_y).setWidth(margin_x).moveTo(g).setValue(this.getOutputMode())
-			lstOutputMode = cp5.addScrollableList(name + "_output mode")
-					.setLabel(Parameter.getOutputModeName(this.getOutputMode()))
-					.addItems(new String[] { "abs", "0-1", "sin" })
-					.setPosition(pos_x, pos_y)
-					.setWidth(margin_x * 3)
-					.setBarHeight(20)
-					.moveTo(g)
-					.onLeave(cf.close)
-					.onEnter(cf.toFront)
-					.close();		
-			pos_x += lstOutputMode.getWidth() + margin_x/2;
-			
-			lstOutputMode.addListenerFor(ScrollableList.ACTION_BROADCAST, new CallbackListener() {
-				@Override
-				public void controlEvent(CallbackEvent theEvent) {
-					setOutputMode((int)((ScrollableList)theEvent.getController()).getValue());	// assumes that abs, 0-1, sin enumerated to 0, 1, 2
-				}
-			});
-			
-			//final FormulaCallback fc = (FormulaCallback) c; 
-				
-			final ScrollableList lstTarget = cp5.addScrollableList(name + "_Target URL")
-					//.addItem(((FormulaCallback)c).targetPath, ((FormulaCallback)c).targetPath)
-					.setLabel(this.getTargetPath()) //((FormulaCallback)c).targetPath)
-					.addItems(APP.getApp().pr.getTargetURLs().keySet().toArray(new String[0]))
-					.setPosition(pos_x, pos_y)
-					.setWidth((cp5.papplet.width/3))
-					.setBarHeight(20).setItemHeight(16)
-					.moveTo(g)
-					.onLeave(cf.close)
-					.onEnter(cf.toFront)
-					.close();
-			
-			pos_x += lstTarget.getWidth() + margin_x/2;
-			
-			
-			//lstTarget.setValue(targetPath);
-			
-			lstTarget.addListenerFor(ScrollableList.ACTION_BROADCAST, new CallbackListener () {
-				@Override
-				synchronized public void controlEvent(CallbackEvent theEvent) {
-					// TODO Auto-generated method stub
-					Map<String, Object> s = ((ScrollableList) theEvent.getController()).getItem((int)lstTarget.getValue());
-					//s.entrySet();
-					//TODO: might not be a Parameter,could just be a Targetable
-					Parameter p = (Parameter) host.host.getObjectForPath((String)s.get("text"));
+
+
+
+		final ScrollableList lstTarget = cp5.addScrollableList(name + "_Target URL")
+				//.addItem(((FormulaCallback)c).targetPath, ((FormulaCallback)c).targetPath)
+				.setLabel(this.getTargetPath()) //((FormulaCallback)c).targetPath)
+				.addItems(APP.getApp().pr.getTargetURLs().keySet().toArray(new String[0]))
+				.setPosition(pos_x, pos_y)
+				.setWidth((cp5.papplet.width/3))
+				.setBarHeight(20).setItemHeight(16)
+				.moveTo(g)
+				.onLeave(cf.close)
+				.onEnter(cf.toFront)
+				.close();
+
+		pos_x += lstTarget.getWidth() + margin_x/2;
+
+
+		//lstTarget.setValue(targetPath);
+
+		lstTarget.addListenerFor(ScrollableList.ACTION_BROADCAST, new CallbackListener () {
+			@Override
+			synchronized public void controlEvent(CallbackEvent theEvent) {
+				// TODO Auto-generated method stub
+				Map<String, Object> s = ((ScrollableList) theEvent.getController()).getItem((int)lstTarget.getValue());
+				//s.entrySet();
+				//TODO: might not be a Parameter,could just be a Targetable
+				String targetPath = (String)s.get("text");
+				Targetable target = (Targetable) host.host.getObjectForPath(targetPath);
+				if (target instanceof Parameter) {
+					Parameter p = (Parameter) target;//(Parameter) host.host.getObjectForPath((String)s.get("text"));
 					synchronized(sequence) {
 						sequence.setHost(((Filter)host.host.getObjectForPath(p.getFilterPath())).sc);
 						sequence.setTargetPath(p.getPath());
 						//sequence.filterPath = p.getFilterPath();
 						//sequence.parameterName = p.getName();//((String) s.get("text")).substring(((String)s.get("text")).lastIndexOf('/')); //)//t.getsetTargetPath((String) s.get("text"));
 					}
-				}				
-			});
-			
-
-			CallbackListener pasteTargetListener = new CallbackListener () {
-				@Override
-				public void controlEvent(CallbackEvent theEvent) {
-		
-					if (cf.control().papplet.mouseButton==(VurfEclipse.MOUSE_RIGHT)) {
-						((ChangeParameterSequence) sequence).setTargetPath((String) APP.getApp().pr.getSequencer().getSelectedTargetPath());
-						lstTarget.setLabel(APP.getApp().pr.getSequencer().getSelectedTargetPath());
+				} else if (target instanceof Scene) {
+					synchronized(sequence) {
+						sequence.setHost((Scene)target);
+						sequence.setTargetPath(targetPath);
+					}
+				} else if (target instanceof Filter) {
+					synchronized(sequence) {
+						sequence.setHost(((Filter)target).sc);
+						sequence.setTargetPath(targetPath);
 					}
 				}
-			};
-			txtExpression.addListenerFor(ScrollableList.ACTION_RELEASE, pasteTargetListener);
-			
-			g.add(lstTarget);		
+			}				
+		});
+
+		Textfield txtExpression = cp5.addTextfield(name + "_Expression")
+				.setText(this.getExpression())
+				.setPosition(pos_x, pos_y)
+				.moveTo(g).setLabel("Expression")
+				.setAutoClear(false);
+
+		pos_x += txtExpression.getWidth() + margin_x;
+
+		// TODO: add callback handler to actually set expression
+
+		CallbackListener setExpression = new CallbackListener() {
+			public void controlEvent(CallbackEvent theEvent) {
+				//((ScrollableList)theEvent.getController()).close();
+				println("Setting new expression: " + ((Textfield)theEvent.getController()).getText());
+				setExpression(((Textfield)theEvent.getController()).getText());
+				((Textfield)theEvent.getController()).setValueLabel(sequence.getExpression());
+			}
+		};
+		txtExpression.addListenerFor(txtExpression.ACTION_BROADCAST,setExpression);
+
+		g.add(txtExpression);
+
+
+		//tglOutputMode = cp5.addToggle(name + "_outputmode_toggle").setPosition(pos_x, pos_y).setWidth(margin_x).moveTo(g).setValue(this.getOutputMode())
+		lstOutputMode = cp5.addScrollableList(name + "_output mode")
+				.setLabel(Parameter.getOutputModeName(this.getOutputMode()))
+				.addItems(new String[] { "abs", "0-1", "sin" })
+				.setPosition(pos_x, pos_y)
+				.setWidth(margin_x * 3)
+				.setBarHeight(20)
+				.moveTo(g)
+				.onLeave(cf.close)
+				.onEnter(cf.toFront)
+				.close();		
+		pos_x += lstOutputMode.getWidth() + margin_x/2;
+
+		lstOutputMode.addListenerFor(ScrollableList.ACTION_BROADCAST, new CallbackListener() {
+			@Override
+			public void controlEvent(CallbackEvent theEvent) {
+				setOutputMode((int)((ScrollableList)theEvent.getController()).getValue());	// assumes that abs, 0-1, sin enumerated to 0, 1, 2
+			}
+		});
+
+		//final FormulaCallback fc = (FormulaCallback) c; 
+
+
+		CallbackListener pasteTargetListener = new CallbackListener () {
+			@Override
+			public void controlEvent(CallbackEvent theEvent) {
+
+				if (cf.control().papplet.mouseButton==(VurfEclipse.MOUSE_RIGHT)) {
+					((ChangeParameterSequence) sequence).setTargetPath((String) APP.getApp().pr.getSequencer().getSelectedTargetPath());
+					lstTarget.setLabel(APP.getApp().pr.getSequencer().getSelectedTargetPath());
+				}
+			}
+		};
+		txtExpression.addListenerFor(ScrollableList.ACTION_RELEASE, pasteTargetListener);
+
+		g.add(lstTarget);		
 		//seq.setHeight(30);
-		
+
 		//pos_y += lstTarget.getHeight() + margin_x/2;
-		
+
 
 		lblInputValue = cf.control().addLabel(name + "_input_indicator").setPosition(pos_x, pos_y - 5).moveTo(g);
 		//pos_x += lblInputValue.getWidth() + margin_x;
 		pos_x += 52;
 		lblOutputValue = cf.control().addLabel(name + "_output_indicator").setPosition(pos_x, pos_y + 5).moveTo(g);
-		
+
 		g.add(lblInputValue);
 		g.add(lblOutputValue);
-			
+
 		//sequenceEditor.setBackgroundHeight(sequenceEditor.getBackgroundHeight() + y);
 		g.setBackgroundHeight(g.getBackgroundHeight());// + 20);
 
-		
+
 		return sequenceEditor;
 	}
 
 	/*public String getParameterPath() {
 		return this.filterPath + "/pa/" + this.parameterName;
 	}*/
-	
+
 	/*public void setParameterPath(String path) {
 		Parameter p = (Parameter) host.host.getObjectForPath(path);
 		parameterName = p.getName();
 		filterPath = p.getFilterPath();
 	}*/
-	
+
+
+	protected void setLatching(boolean b) {
+		this.latching = b;
+	}
 
 	@Override
 	public String toString() {
@@ -382,7 +460,7 @@ public class ChangeParameterSequence extends Sequence {
 		}
 		return false;
 	}
-	
+
 
 	public void preserveCurrentParameters() {
 		super.preserveCurrentParameters();
